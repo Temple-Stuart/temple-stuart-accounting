ae58f8c5cf1ce47676f17cc3c6d6436c1d20e2a2
Temple Stuart <astuart@templestuart.com>
2025-09-04 21:25:59 -0700

    Add logo and social media icons to header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Header from '@/components/navigation/Header';
 import HeroSection from '@/components/sections/HeroSection';
 import BookkeepingSection from '@/components/sections/BookkeepingSection';
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <HeroSection />
       <BookkeepingSection />
     </div>
