0a5d7ea1595d5e125090f2da826fed5088ad7612
Temple Stuart <astuart@templestuart.com>
2025-09-05 20:32:06 -0700

    Add separate LoginSection component, restructure homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
 import Header from '@/components/navigation/Header';
 import HeroSection from '@/components/sections/HeroSection';
 import BookkeepingSection from '@/components/sections/BookkeepingSection';
+import LoginSection from '@/components/sections/LoginSection';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <BookkeepingSection />
+      <LoginSection />
+    </div>
+  );
+}import Header from '@/components/navigation/Header';
+import HeroSection from '@/components/sections/HeroSection';
+import BookkeepingSection from '@/components/sections/BookkeepingSection';
 
 export default function Home() {
   return (
