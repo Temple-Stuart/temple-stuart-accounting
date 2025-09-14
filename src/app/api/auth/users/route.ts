129ef202569b4b192b3394acd34ee93a5a4b53b4
Temple Stuart <astuart@templestuart.com>
2025-09-13 22:11:01 -0700

    Fix user to users in users route

--- src/app/api/auth/users/route.ts
+++ src/app/api/auth/users/route.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaClient()
 
 export async function GET() {
   try {
-    const users = await prisma.user.findMany()
+    const users = await prisma.users.findMany()
     return new Response(JSON.stringify(users), { status: 200 })
   } catch (err) {
     return new Response(JSON.stringify({ error: 'Failed to fetch users' }), { status: 500 })
