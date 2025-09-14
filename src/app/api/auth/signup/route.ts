8a2ad7127ab30afa935c984a876d5fb872a7b1d7
Temple Stuart <astuart@templestuart.com>
2025-09-13 22:09:17 -0700

    Fix user to users in signup route

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient()
 export async function POST(req: Request) {
   try {
     const body = await req.json()
-    const user = await prisma.user.create({
+    const user = await prisma.users.create({
       data: body,
     })
     return new Response(JSON.stringify(user), { status: 201 })
